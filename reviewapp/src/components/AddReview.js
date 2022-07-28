9f2835b2230de03acdbb1919b9c56972613ee447
Eyal Yosipovich <eyalyosi@gmail.com>
2022-07-28 12:37:14 +0300

    add function on ReviewPopup cmp

--- reviewapp/src/components/AddReview.js
+++ reviewapp/src/components/AddReview.js
@@ -1,16 +1,13 @@
 import styled from 'styled-components'
 
-
-const Section = styled.section`
+const Section = styled.form`
 display: flex;
 flex-direction: column;
 align-items: flex-start;
 padding: 18px 20px 20px;
 gap: 15px;
-
 width: 430px;
 height: 132px;
-
 background: #FFFFFF;
 box-shadow: 0px 5px 20px rgba(0, 0, 0, 0.5);
 border-radius: 10px;
@@ -21,49 +18,39 @@ display: flex;
 flex-direction: row;
 justify-content: space-between;
 align-items: center;
-padding: 0px;
 gap: 10px;
-
-width: 390px;
-height: 18px;
+width: 100%;
 `
-const TitleCount = styled.div`
-width: 50px;
-height: 18px;
 
+const TitleCount = styled.div`
 font-family: 'Assistant';
 font-style: normal;
 font-weight: 400;
 font-size: 14px;
 line-height: 18px;
-
 display: flex;
 align-items: center;
 text-align: right;
-
-color: #64757E;
-
+color: #C4D2DC;
+span {
+    color: #64757E;
+}
 `
 
 const InputTitle = styled.input`
 width: 330px;
-height: 18px;
-
-/* Body/Body bold */
-
 font-family: 'Assistant';
 font-style: normal;
 font-weight: 700;
 font-size: 14px;
 line-height: 18px;
-/* identical to box height, or 129% */
-
 display: flex;
 align-items: center;
-
-/* Neutral/500 */
 border: 0px;
-color: #C4D2DC;
+color: #39454C;
+::placeholder {
+    color: #C4D2DC;
+}
 `
 
 const TextFrame = styled.div`
@@ -71,46 +58,31 @@ display: flex;
 flex-direction: row;
 justify-content: space-between;
 align-items: center;
-padding: 0px;
 gap: 10px;
-
-width: 390px;
+width: 100%;
 height: 45px;
 `
 
 const InputText = styled.input`
 width: 313px;
 height: 18px;
-
-/* Body/Body */
-
 font-family: 'Assistant';
 font-style: normal;
 font-weight: 400;
 font-size: 14px;
 line-height: 18px;
-/* identical to box height, or 129% */
-
 display: flex;
 align-items: center;
-
-/* Neutral/500 */
 border: 0px;
-color: #C4D2DC;
+color: #39454C;
+::placeholder {
+    color: #C4D2DC;
+}
 `
 
 const AddButton = styled.button`
-display: flex;
-flex-direction: row;
-justify-content: center;
-align-items: center;
-padding: 10px 20px 11px;
-gap: 5px;
-
 width: 64px;
 height: 45px;
-
-/* Neutral/400 */
 border: 0px;
 background: #FF206F;
 border-radius: 10px;
@@ -118,34 +90,28 @@ color: #FFFFFF;
 &:hover {
     cursor: pointer
 }
-
 `
 
 const Hr = styled.hr`
-width: 390px;
-height: 1px;
-
+border: 1px solid #EBEFF7;
+width: 100%;
 background: #EBEFF7;
-
 `
 
-const AddReview = ({ title, setTitle, text ,setText}) => {
-
+const AddReview = ({ title, onChangeTitle, text, setText, add }) => {
     return (
-        <Section>
+        <Section action="" onSubmit={add}>
             <TitleFrame>
-                <InputTitle type="text" placeholder='Enter review title' value={title} onChange={(event) => setTitle(event.target.value)} />
-                <TitleCount>0/255</TitleCount>
+                <InputTitle type="text" placeholder='Enter review title' value={title} onChange={(event) => onChangeTitle(event.target.value)} />
+                <TitleCount><span>{title.length}</span>/255</TitleCount>
             </TitleFrame>
             <Hr />
             <TextFrame>
-                <InputText type="text" placeholder='Write your review text...' value={text} onChange={(event) => setText(event.target.value)}/>
+                <InputText type="text" placeholder='Write your review text...' value={text} onChange={(event) => setText(event.target.value)} />
                 <AddButton>Add</AddButton>
             </TextFrame>
         </Section>
     )
-
 }
-
 
 export default AddReview
--- reviewapp/src/components/ReviewPopup.js
+++ reviewapp/src/components/ReviewPopup.js
@@ -6,38 +6,38 @@ import { useEffect, useState } from 'react';
 import { reviewService } from '../services/ReviewService'
 
 const Section = styled.section`
-
 display: flex;
 flex-direction: column;
 align-items: flex-start;
 padding: 20px 20px 0px;
-
 width: 470px;
 height: 549px;
-
-
 background: linear-gradient(72.2deg, #39454C 0%, #64757E 100%);
 border-radius: 20px 0px 0px 20px;
-
-/* Inside auto layout */
-
-flex: none;
-order: 0;
-align-self: stretch;
-flex-grow: 0;
 `
 
 const ReviewPopup = () => {
     const [title, setTitle] = useState('')
     const [text, setText] = useState('')
     const [reviews, setReviews] = useState([])
-  
+
+    const onChangeTitle = (value) => {
+        if (value.length > 255) return
+        setTitle(value);
+    }
+    
+    const add = async (event) => {
+        event.preventDefault();
+        await reviewService.add({ title, text })
+        setText('')
+        setTitle('')
+    }
+
     return (
         <Section>
-            <AddReview title={title} setTitle={setTitle} text={text} setText={setText}/>
+            <AddReview title={title} onChangeTitle={onChangeTitle} text={text} setText={setText} add={add}/>
         </Section>
     )
-
 }
 
 
